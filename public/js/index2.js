2f3f066d5cbca501ecc3721d33647ac2f182b97f
joa32660 <jos3266@naver.com>
2019-11-12 01:34:16 +0900

    요일 수정

--- public/js/index2.js
+++ public/js/index2.js
@@ -1,22 +1,79 @@
-//전역변수
-var ajax = new XMLHttpRequest();
-var dailyAjax = new XMLHttpRequest();
-var weeklyAjax = new XMLHttpRequest();
+//전역변수선언
 var key = 'b06f5fcc28a3c5dd4be148aed70d266b';
+var units = 'metric';
 var dailyAPI = 'https://api.openweathermap.org/data/2.5/weather';
 var weeklyAPI = 'https://api.openweathermap.org/data/2.5/forecast';
+var cityURL = '../json/city.json';
+var dailyURL = dailyAPI + '?appid=' + key + "&units=" + units;
+var weeklyURL = weeklyAPI + '?appid=' + key + "&units=" + units;
 
-cityInit();
-function cityInit() {
-	ajax.onreadystatechange = function () {
+
+init();
+
+function init() {
+	wrapChg("M");
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = cityFn;
+	ajax.open('GET', cityURL, true);
+	ajax.send();
+}
+
+//네비게이션 이벤트 처리
+for(var v of document.querySelectorAll(".navi > li")){
+	v.addEventListener("click", (e) =>{
+		switch(e.target.outerText){
+			case "DAILY":
+				wrapChg("D");
+				break;
+			case "WEEKLY":
+				wrapChg("W");
+				break;
+			default:
+			 init();
+			 break;
+		}
+	});
+}
+
+function wrapChg(type) {
+	const _daily = document.querySelector(".daily");
+	const _weekly = document.querySelector(".weekly");
+	const _main = document.querySelector(".main");
+	switch (type) {
+		case "D":
+			_daily.classList.add("d-block");
+			_daily.classList.remove("d-none");
+			_weekly.classList.add("d-none");
+			_weekly.classList.remove("d-block");
+			_main.classList.add("d-none");
+			_main.classList.remove("d-block");
+			break;
+		case "W":
+			_daily.classList.add("d-none");
+			_daily.classList.remove("d-block");
+			_weekly.classList.add("d-block");
+			_weekly.classList.remove("d-none");
+			_main.classList.add("d-none");
+			_main.classList.remove("d-block");
+			break;
+		default:
+			_daily.classList.add("d-none");
+			_daily.classList.remove("d-block");
+			_weekly.classList.add("d-none");
+			_weekly.classList.remove("d-block");
+			_main.classList.add("d-block");
+			_main.classList.remove("d-none");
+			break;
+	}
+}
+
+function cityFn() {
 		if (this.readyState == 4 && this.status == 200) {
-			var cities = JSON.parse(this.responseText).cities;
-			//id로 DOM 접근하기 - jQuery
-			var $citySelect = $("#cities");
-			//id로 DOM 접근하기 - ES5
-			var citySelect5 = document.getElementById("cities");
-			//id로 DOM 접근하기 - ES6
-			var citySelect = document.querySelector("#cities");
+			var _city = document.querySelector("#cities");
+			var res = JSON.parse(this.responseText).cities;
+			_city.innerHTML = '<option value="" selected>Please choose a city.</option>';
+			for(var v of res) _city.innerHTML += `<option value="${v.id}">${v.name}</option>`
+			/*
 			var elem;
 			var cityName;
 			elem = document.createElement('option'); 	
@@ -34,7 +91,7 @@ function cityInit() {
 			}
 
 			//jQuery select change 이벤트
-			/*
+			
 			$("#cities").change(function(){
 				$(this).val();
 			});
@@ -42,20 +99,79 @@ function cityInit() {
 
 			//ES5, ES6 change 이벤트
 			//citySelect.addEventListener(이벤트, 콜백함수);
-			citySelect.addEventListener("change", function(){
-				var cityId = this.value;
-				var dailyURL = dailyAPI + "?appid=" + key + "&id=" + cityId;
-				dailyAjax.onreadystatechange = function(){
-					if (this.readyState == 4 && this.status == 200) {
-						var daily = JSON.parse(this.responseText);
-						console.log(daily);
-					}
-				};
-				dailyAjax.open("GET", dailyURL, true);
-				dailyAjax.send();
+			_city.addEventListener("change", function(e){
+				var ajax = new XMLHttpRequest();
+				ajax.onreadystatechange = dailyFn;
+				ajax.open('GET', dailyURL + "&id=" + e.target.value, true);
+				ajax.send();
+				var ajax2 = new XMLHttpRequest();
+				ajax2.onreadystatechange = weeklyFn;
+				ajax2.open('GET', weeklyURL + "&id=" + e.target.value, true);
+				ajax2.send();
 			});
 		}
-	};
-	ajax.open("GET", "../json/city.json", true);
-	ajax.send();
+}
+
+//데일리 정보 가져오기
+function dailyFn(){
+	if(this.readyState == 4 && this.status == 200){
+		let res = JSON.parse(this.responseText);
+		var wts = new Date(new Date(v.dt_txt).getTime() + (9*60*60*1000));
+		let iconSrc = `../img/icon/${res.weather[0].icon}.png`;
+		let desc = `<b>${res.weather[0].main}</b> `;
+		let temp = `| &nbsp;&nbsp;&nbsp;&nbsp;<b>${res.main.temp}</b>˚C`;
+		let time = `<span>${dspDate(wts, 5)}</span>`;
+		let _wrap = document.querySelector(".daily").querySelector(".conts");
+		let _title = document.createElement("div");
+		let _img = document.createElement("div");
+		let _temp = document.createElement("span");
+		let _desc = document.createElement("span");
+		let _time = document.createElement("div");
+		_title.innerHTML = 'Today weather';
+		_img.innerHTML = `<img src="${iconSrc}" class="w-100 daily-img">`;
+		_temp.innerHTML = temp;
+		_desc.innerHTML = desc;
+		_time.innerHTML = time;
+		_title.setAttribute("class","text-center py-3 fa-3x w-tit");
+		_img.setAttribute("class","text-center py-3");
+		_desc.setAttribute("class","text-center py-3 d-inline-block f-2");
+		_temp.setAttribute("class","text-center py-3 d-inline-block f-2");
+		_time.setAttribute("class","text-center py-3 d-inline-block f-2");
+		_wrap.innerHTML = '';
+		_wrap.appendChild(_title);
+		_wrap.appendChild(_img);
+		_wrap.appendChild(_desc);
+		_wrap.appendChild(_temp);
+		_wrap.appendChild(_time);
+		wrapChg("D");
+	}
+}
+
+//위클리 정보 가져오기
+function weeklyFn(){
+	if (this.readyState == 4 && this.status == 200) {
+		let res = JSON.parse(this.responseText);
+		var kts;
+	  var html = '';
+	  var _conts = document.querySelector(".weekly > .conts");
+	  _conts.innerHTML = '';
+	 for(var v of res.list){
+		kts = new Date(new Date(v.dt_txt).getTime() + (9*60*60*1000));
+		html = `
+	 <li class="w-item">
+		<div>
+		<img src="../img/icon/${v.weather[0].icon}.png" alt="" class="w-100">
+		</div>
+		<ul>
+		<li class="w-temp"><span>${v.main.temp}</span>˚C</li>
+		<li class="w-desc">
+		<span>${v.weather[0].main}</span>
+		<span>${v.weather[0].description}</span>
+		</li>
+		<li class="w-date">${dspDate(kts, 2)}forecast</li>
+		</ul>
+	 </li>`;
+		_conts.innerHTML += html;
+	 }
+	}
 }
--- public/js/util.js
+++ public/js/util.js
@@ -1,12 +1,14 @@
 // '2019년 8월 11일 9시 8분 11초' 형식으로 보내주는 함수
 function dspDate(d, type=0){
 	var monthArr = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"];
+	var weekArr = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY, FRIDAY, SATURDAY"];
 	var year = d.getFullYear() + "년"; //2019
 	var month = monthArr[d.getMonth()]+" "; // 7 (0~11)
 	var day = d.getDate() +"일"; //23
 	var hour = d.getHours() + "시"; // 0~23
 	var min = d.getMinutes() + "분"; //0~59
 	var sec = d.getSeconds() + "초"; //0~59
+	var week = weekArr[d.getDay()]+" "; //0~6
 	var returnStr;
 	/*
 	type 0: 2019년 8월 11일 9시 8분 11초
@@ -14,6 +16,7 @@ function dspDate(d, type=0){
 	type 2: 2019년 8월 11일 9시
 	type 3: 2019년 8월 11일
 	type 4: 8월 11일
+	type 5: MONDAY
 	*/
 
 	switch(type){
@@ -29,10 +32,12 @@ function dspDate(d, type=0){
 		case 4:
 				returnStr = month + day;
 			break;
+		case 5:
+				returnStr = week;
+			break;
 		default:
-				returnStr = year + month + day + hour + min + sec;
+				returnStr = year + month + day + week + hour + min + sec;
 			break;
 	}
-
-	return year + month + day + hour + min + sec;
+	return year + month + day + week + hour + min + sec;
 }
